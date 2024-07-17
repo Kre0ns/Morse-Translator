c4e8b7082852e59325f78a4ca57cc2922dce5af5
Kre0ns <yonpun13@gmail.com>
2024-07-17 20:10:49 +0300

    Added a function to decipher the morse code.

--- Morse-Translator.py
+++ Morse-Translator.py
@@ -1,3 +1,10 @@
+MORSE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..'
+    }
+
 def UserInput():
     # Gets user input and processes it
 
@@ -10,4 +17,32 @@ def UserInput():
     # Spliting the string to individual letters and spaces
     processedUserInput = respacedUserInput.split("/")
 
+    # Debug: print(processedUserInput)
     return processedUserInput
+
+
+def DecipherMorse(morseCode):
+    # Converts the morse into a string
+
+    decipheredLetterList = []
+
+    decipheredString = ""
+
+    for entry in morseCode:
+
+        # Checks if the current entry is a space and if it is, it appends it to the decipheredLetterList
+        if entry == " ":
+
+            decipheredLetterList.append(entry)
+
+        else:
+
+            # Checks the MORSE_DICT for the translation of a morse string and appends it to the decipheredLetterList
+            decipheredLetter = list(MORSE_DICT.keys())[list(MORSE_DICT.values()).index(entry)]
+            decipheredLetterList.append(decipheredLetter)
+
+    #Joins the list into a single string (decipheredString)
+    decipheredString = "".join(decipheredLetterList)
+
+    # Debug: print(decipheredString)
+    return decipheredString
