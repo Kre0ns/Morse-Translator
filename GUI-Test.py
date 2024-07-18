375dffc30ab4845948646cb9ae9d033251192caf
Kre0ns <yonpun13@gmail.com>
2024-07-18 11:49:46 +0300

    Redid the GUI using a new lib.

--- GUI-Test.py
+++ GUI-Test.py
@@ -1,16 +1,24 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter
+import customtkinter
 
-root = Tk()
+window = customtkinter.CTk()
+window.title("Morse Translator")
+window.resizable(False, False)
+window.columnconfigure(0, weight=1)
 
-root.title("Morse Translator")
+label1 = customtkinter.CTkLabel(window, font=("Arial", 13.4), text="Type the morse code you wish to translate (Dots . and dashes -)(Formating: Seperate letters with / and words with #):")
+label1.grid(row=0, column=0, padx=10, sticky="NSW")
 
-mainFrame = ttk.Frame(root, padding=10)
-mainFrame.grid()
+inputTextBox = customtkinter.CTkTextbox(window, wrap="word", height=150)
+inputTextBox.grid(row=1, column=0, padx=10, pady=10, sticky="NSEW")
 
-ttk.Label(mainFrame, text="Type the morse code you wish to translate (Dots . and dashes -)(Formating: Seperate letters with / and words with #):", font=30).grid(column=0, row=0, sticky=(N,S,W,E))
-ttk.Entry(mainFrame, font=30).grid(column=0, row=1, sticky=(N,S,W,E))
-ttk.Button(mainFrame, text="Translate").grid(column=0, row=2)
-ttk.Label(mainFrame, text="Translation will apper here.", font=30).grid(column=0, row=3, sticky=(N,S,W,E))
+translateButton = customtkinter.CTkButton(window, font=("Arial", 13.4), text="Translate")
+translateButton.grid(row=2, column=0, padx=10, pady=10, sticky="NSEW")
 
-root.mainloop()
+label1 = customtkinter.CTkLabel(window, font=("Arial", 13.4), text="The translation will appear here:")
+label1.grid(row=3, column=0, padx=10, sticky="NSW")
+
+outputTextBox = customtkinter.CTkTextbox(window, wrap="word", height=150)
+outputTextBox.grid(row=4, column=0, padx=10, pady=10, sticky="NSEW")
+
+window.mainloop()
